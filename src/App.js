a1e8b2e8e268f8b4aaabeb06b2f86e80ace488b0
Paquitosoft <paquitosoftware@gmail.com>
2020-03-19 13:49:38 +0100

    Add conditional render to App main component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
+
+import { getValue } from './plugins/local-cache';
+import { CACHE } from './constants';
+
 import Layout from './components/layout/layout';
 import AccessView from './components/views/access-view/access-view';
 
-import './App.css';
+import './app.css';
 
 function App() {
+	const userToken = getValue(CACHE.userToken);
+	console.log({ userToken, foo: !!userToken });
 	return (
-		<Layout />
+		<div>
+			{!!userToken ? <Layout /> : <AccessView />}
+		</div>
 	);
 }
 
