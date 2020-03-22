330f0eecb1546d04c4bbd9ce6a150cb737f10219
Paquitosoft <paquitosoftware@gmail.com>
2020-03-22 18:20:02 +0100

    Normalize tracks duration format

--- src/components/views/playlist-detail-view/track-row/track-row.js
+++ src/components/views/playlist-detail-view/track-row/track-row.js
@@ -8,6 +8,20 @@ import { ReactComponent as TrackMenuIcon } from './add-icon.svg';
 
 import './track-row.css';
 
+const DURATION_REGEXP = /\(?(\d{2}):(\d{2})/;
+
+function getDuration(trackDuration) {
+	console.log(trackDuration, typeof trackDuration);
+	let seconds = parseInt(trackDuration, 10);
+	if (!seconds) {
+		const[_, _minutes, _seconds] = trackDuration.match(DURATION_REGEXP);
+		seconds = (parseInt(_minutes, 10) * 60) + parseInt(_seconds, 10);
+	}
+	const minutes = `${Math.floor(seconds / 60)}`.padStart(2, '0');
+  	const _seconds = `${seconds % 60}`.padStart(2, '0');
+  	return `${minutes}:${_seconds}`;
+}
+
 function TrackRow({ track, index }) {
 	const FavoriteIcon = track.isFavorite ? FavoriteYesIcon : FavoriteNoIcon;
 	return (
@@ -18,7 +32,7 @@ function TrackRow({ track, index }) {
 				<FavoriteIcon className="track-row__favorite-icon icon" />
 			</div>
 			<div className="track-row__title" title={track.title}>{track.title}</div>
-			<div className="track-row__duration">{track.duration}</div>
+			<div className="track-row__duration">{getDuration(track.duration)}</div>
 			<div className="track-row__actions">
 				<IconButton><TrackMenuIcon/></IconButton>
 			</div>
