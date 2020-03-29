c6449ce46a5994e0247a73309413f79323f6eacb
Paquitosoft <paquitosoftware@gmail.com>
2020-03-29 16:44:09 +0200

    Added playslits filtering to PlaylistsView

--- src/components/views/playlists-view/playlists-view.js
+++ src/components/views/playlists-view/playlists-view.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import useDataFetching from '../../shared/use-data-fetching/use-data-fetching';
 
 import Layout from '../../layout/layout';
 import PlaylistSummary from './playlist-summary/playlist-summary';
+import FormInput from '../../shared/form-input/form-input';
 
 import './playlists-view.css';
 
@@ -20,18 +21,45 @@ const PLAYLISTS_QUERY = `
 
 function PlaylistsView() {
 	const { isFetching, data } = useDataFetching({ query: PLAYLISTS_QUERY });
-	const playlists = data && data.getPlaylistsByUser;
+	const [playlists, setPlaylists] = useState([]);
+	const [filteredPlaylists, setFilteredPlaylists] = useState([]);
+	const playlistsList = data && data.getPlaylistsByUser;
+
+	useEffect(() => {
+		if (playlistsList) {
+			setPlaylists(playlistsList);
+			setFilteredPlaylists(playlistsList);
+		};
+	}, [playlistsList]);
+
+	const filterPlaylists = (filter => {
+		const regexp = new RegExp(filter || '.*', 'i');
+		setFilteredPlaylists(playlists.filter(playlist => regexp.test(playlist.name)));
+	});
 
 	return (
 		<Layout>
 			<div className="playlists-view">
-				
-				<h1 className="playlists-view__title">YOUR PLAYLISTS</h1>
+				<div className="playlist-view__header">
+					<div className="playlists-view__title_wrapper">
+						<h1 className="playlists-view__title">YOUR PLAYLISTS</h1>
+						{filteredPlaylists.length > 0 &&
+							<span className="playlists-view__count">({filteredPlaylists.length})</span>
+						}
+					</div>
+					<FormInput 
+						className="playlists-view__filter" 
+						type="text" 
+						name="filter"
+						placeholder="Filter"
+						onChange={({ target }) => filterPlaylists(target.value)}
+					/>
+				</div>
 				{isFetching && <div className="playlists-view__loading">Loading playlists...</div>}
 				{
-					!!playlists &&
+					filteredPlaylists.length > 0 &&
 					<div className="playlists-view__list">
-						{playlists.map((playlist, index) => <PlaylistSummary playlist={playlist} key={index} />)}
+						{filteredPlaylists.map((playlist, index) => <PlaylistSummary playlist={playlist} key={index} />)}
 					</div>
 				}
 			</div>
