0b6e3c075e2a98eb78ea957e4187b95922ac5830
Paquitosoft <paquitosoftware@gmail.com>
2020-03-31 00:39:10 +0200

    First steps in user creation and password reset

--- src/components/views/access-view/forgot-password-form/forgot-password-form.js
+++ src/components/views/access-view/forgot-password-form/forgot-password-form.js
@@ -1,17 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { getDataFromForm } from '../../../../plugins/form-helpers';
+import { useAppContext } from '../../../shared/app-context/app-context';
 
 import FormInput from '../../../shared/form-input/form-input';
 import Button from '../../../shared/button/button';
 import Link from '../../../shared/link/link';
 
 import './forgot-password-form.css';
+import Alert from '../../../shared/alert/alert';
+
+const RESET_PASSWORD_MUTATION = `
+	mutation ResetPassword($email: String!) {
+		resetPassword(email: $email)
+	}
+`;
 
 function ForgotPasswordForm({ onBackClicked }) {
+	const [isWorking, setIsWorking] = useState(false);
+	const [formError, setFormError] = useState(null);
+	const { apiClient } = useAppContext();
+
+	const onSubmit = (event) => {
+		event.preventDefault();
+		const { email } = getDataFromForm(event.target);
+		
+		setIsWorking(true);
+		apiClient.sendMutation({
+			mutation: RESET_PASSWORD_MUTATION,
+			variables: { email }
+		})
+		.then(({ data: { createUser: result} }) => {
+			setIsWorking(false);
+			setFormError(null);
+			// TODO Show success message
+		})
+		.catch(([error]) => {
+			setIsWorking(false);
+			setFormError(error);
+			// Ups! It seems that's not your password
+		});
+	}
+	
 	return (
 		<div className="forgot-password-form">
 			<p className="forgot-password-form__title">Recover your password</p>
 			<p className="forgot-password-form__subtitle">Type your email and we will send you a message with a link to set a new password for your account</p>
-			<form action="/api/reset-password" className="forgot-password-form__form">
+			<form action="/api/reset-password" className="forgot-password-form__form" onSubmit={onSubmit}>
 				<FormInput 
 					className="forgot-password-form__form-input" 
 					type="text" 
@@ -22,7 +57,9 @@ function ForgotPasswordForm({ onBackClicked }) {
 				<Button 
 					className="forgot-password-form__login-button" 
 					kind="primary"
+					isWorking={isWorking}
 				>RESET PASSWORD</Button>
+				{!!formError && <Alert className="forgot-password-form__error">{formError.message}</Alert>}
 				<Link 
 					href="/login" 
 					className="forgot-password-form__back"
